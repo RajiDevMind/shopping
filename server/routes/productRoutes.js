62037f8f6799350afe5ac8157d768dabbc7fbe27
Raji Opeyemi Ibrahim <omoiyaalasso@gmail.com>
2024-05-08 13:45:15 +0100

    A logged in user that is an admin can only create a product

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { createProduct } = require("../controllers/productControllers");
 const { auth, adminOnlyAuth } = require("../middleware/authMiddleware");
 
+// a logged in user that is an admin can only create a product
 router.post("/", auth, adminOnlyAuth, createProduct);
 
 module.exports = router;
