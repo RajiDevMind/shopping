17bad252dbe59d950f91db17c8a6222d6a86d929
Raji Opeyemi Ibrahim <omoiyaalasso@gmail.com>
2024-05-15 20:57:53 +0100

    form to create category was created

--- client/src/components/admin/category/CreateCategory.jsx
+++ client/src/components/admin/category/CreateCategory.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "../../card/Card";
+import "./Category.scss";
 
 const CreateCategory = () => {
+  const [category, setCategory] = useState("");
+
+  const saveCategory = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="--mb2">
       <h3>Create Category</h3>
       <p>
         Use this form to <b>create a category</b>
       </p>
+      <Card cardClass={"card"}>
+        <br />
+        <form onSubmit={saveCategory}>
+          <label htmlFor="category">Category Name:</label>
+          <input
+            type="text"
+            name="category"
+            placeholder="Category name"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required
+          />
+          <div className="--my">
+            <button type="submit" className="--btn --btn-primary">
+              Save Category
+            </button>
+          </div>
+        </form>
+      </Card>
     </div>
   );
 };
