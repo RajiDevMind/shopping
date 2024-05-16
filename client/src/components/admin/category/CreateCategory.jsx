776ee5bd70fa7c2549d5d303d44d19e64985f7e5
Raji Opeyemi Ibrahim <omoiyaalasso@gmail.com>
2024-05-16 06:24:20 +0100

    Category was created and sent to server(DB)

--- client/src/components/admin/category/CreateCategory.jsx
+++ client/src/components/admin/category/CreateCategory.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import Card from "../../card/Card";
 import "./Category.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { createCategory } from "../../../redux/features/cat&brands/CatsAndBrandsSlice";
+import { toast } from "react-toastify";
+import Loader from "../../loader/Loader";
 
 const CreateCategory = () => {
   const [category, setCategory] = useState("");
 
+  const { isLoading } = useSelector((state) => state.category);
+  const dispatch = useDispatch();
+
   const saveCategory = async (e) => {
     e.preventDefault();
+    if (category.length < 3) {
+      return toast.error("Category name must be above 3 characters!");
+    }
+    const categoryData = {
+      name: category,
+    };
+    dispatch(createCategory(categoryData));
+    setCategory("");
   };
 
   return (
     <div className="--mb2">
+      {isLoading && <Loader />}
       <h3>Create Category</h3>
       <p>
         Use this form to <b>create a category</b>
