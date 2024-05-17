116ce94d4a891220bd4f6de3c07c24e7e16d8c2d
Raji Opeyemi Ibrahim <omoiyaalasso@gmail.com>
2024-05-17 11:18:37 +0100

    jsx form setup to create product

--- client/src/components/admin/addProduct/AddProduct.jsx
+++ client/src/components/admin/addProduct/AddProduct.jsx
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./AddProduct.scss";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../loader/Loader";
+import ProductForm from "../productForm/ProductForm";
+import {
+  getBrands,
+  getCategories,
+} from "../../../redux/features/cat&brands/CatsAndBrandsSlice";
+
+const initialState = {
+  name: "",
+  category: "",
+  brand: "",
+  quantity: "",
+  color: "",
+  price: "",
+  regularPrice: "",
+};
 
 const AddProduct = () => {
+  const [product, setProduct] = useState(initialState);
+  const [description, setDescription] = useState("");
+  const [filteredBrands, setFilteredBrands] = useState([]);
+
+  const { name, category, brand, quantity, color, price, regularPrice } =
+    product;
+
+  const { isLoading } = useSelector((state) => state.product);
+  const { categories, brands } = useSelector((state) => state.category);
+
+  // holding state to avoid empty data
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCategories());
+    dispatch(getBrands());
+  }, [dispatch]);
+
+  // filter brands based on selectedCategory
+  const filterBrands = (selectedCategory) => {
+    const newBrands = brands.filter(
+      (brand) => brand.category === selectedCategory
+    );
+    setFilteredBrands(newBrands);
+  };
+  useEffect(() => {
+    filterBrands(category);
+  }, [category]);
+
+  const handleInputChange = async (e) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    console.log(product);
+    console.log(description);
+  };
+
   return (
-    <div>
-      <h1>AddProduct</h1>
-    </div>
+    <section>
+      {isLoading && <Loader />}
+      <div className="container">
+        <h3 className="--m3">Add New Product</h3>
+
+        <ProductForm
+          saveProduct={saveProduct}
+          product={product}
+          categories={categories}
+          handleInputChange={handleInputChange}
+          isEditing={false}
+          filteredBrands={filteredBrands}
+          description={description}
+          setDescription={setDescription}
+        />
+      </div>
+    </section>
   );
 };
 
--- client/src/components/admin/productForm/ProductForm.jsx
+++ client/src/components/admin/productForm/ProductForm.jsx
@@ -1,8 +1,154 @@
 import React from "react";
 import "./ProductForm.scss";
+import Card from "../../card/Card";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-const ProductForm = () => {
-  return <div>ProductForm</div>;
+const ProductForm = ({
+  saveProduct,
+  product,
+  handleInputChange,
+  categories,
+  isEditing,
+  filteredBrands,
+  description,
+  setDescription,
+}) => {
+  return (
+    <div className="add-product">
+      <h3>Upload widget placeholder</h3>
+
+      <Card cardClass={"card"}>
+        <br />
+        <form onSubmit={saveProduct}>
+          <label htmlFor="name">Product Name:</label>
+          <input
+            type="text"
+            placeholder="Product name"
+            name="name"
+            value={product?.name}
+            onChange={handleInputChange}
+          />
+          <label>Product Category:</label>
+          <select
+            name="category"
+            value={product?.category}
+            onChange={handleInputChange}
+          >
+            {isEditing ? (
+              <option value={product?.category}>{product?.category}</option>
+            ) : (
+              <option>Select Category</option>
+            )}
+            {categories.length > 0 &&
+              categories.map((category, index) => (
+                <option key={category._id} value={category.name}>
+                  {category.name}
+                </option>
+              ))}
+          </select>
+          {/* Brands start here */}
+          <label>Product Brand:</label>
+          <select
+            name="brand"
+            value={product?.brand}
+            onChange={handleInputChange}
+          >
+            {isEditing ? (
+              <option value={product?.brand}>{product?.brand}</option>
+            ) : (
+              <option>Select brand</option>
+            )}
+            {filteredBrands.length > 0 &&
+              filteredBrands.map((brand, index) => (
+                <option key={brand._id} value={brand.name}>
+                  {brand.name}
+                </option>
+              ))}
+          </select>
+
+          <label htmlFor="name">Product Color:</label>
+          <input
+            type="text"
+            placeholder="Product Color"
+            name="color"
+            value={product?.color}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="name">Regular Price:</label>
+          <input
+            type="number"
+            placeholder="Regular Price"
+            name="regularPrice"
+            value={product?.regularPrice}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="name">Product Price:</label>
+          <input
+            type="number"
+            placeholder="Price"
+            name="price"
+            value={product?.price}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="name">Product Quantity:</label>
+          <input
+            type="number"
+            placeholder="Product Quantity"
+            name="quantity"
+            value={product?.quantity}
+            onChange={handleInputChange}
+          />
+          <label>Description</label>
+          <ReactQuill
+            theme="snow"
+            value={description}
+            onChange={setDescription}
+            modules={ProductForm.modules}
+            formats={ProductForm.formats}
+          />
+
+          <div className="--my">
+            <button type="submit" className="--btn --btn-primary">
+              Save Product
+            </button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
 };
+
+ProductForm.modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+ProductForm.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "color",
+];
 
 export default ProductForm;
