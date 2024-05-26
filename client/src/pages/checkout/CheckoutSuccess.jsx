6a079b5e0beaba6a2d59d58ddd90e27ee3a0330b
Raji Opeyemi Ibrahim <omoiyaalasso@gmail.com>
2024-05-26 11:08:00 +0100

    Checkout Success page, after successful payment. User cart was cleared.

--- client/src/pages/checkout/CheckoutSuccess.jsx
+++ client/src/pages/checkout/CheckoutSuccess.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Confetti from "react-confetti";
+import { useDispatch } from "react-redux";
+import { CLEAR_CART } from "../../redux/features/cart/cartSlice";
 
 const CheckoutSuccess = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(CLEAR_CART());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>CheckoutSuccess</h1>
-    </div>
+    <>
+      <Confetti />
+      <section>
+        <div className="container">
+          <h2>Checkout Successful</h2>
+          <p>Thank you for your purchase as your order is processing!</p>
+          <br />
+          <button className="--btn --btn-primary">
+            <Link to="/order-history">View Order Status</Link>
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
